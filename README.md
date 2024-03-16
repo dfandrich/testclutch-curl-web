993318c895fdd169d5941b09db796d647a533825
Dan Fandrich <dan@coneharvesters.com>
2024-03-15 19:30:38 -0700

    Initial commit of deployment scripts for Test Clutch for curl

---
+++ README.md
@@ -0,0 +1,91 @@
+# Test Clutch deployment for curl
+
+This repository contains the files needed to deploy an instance of Test Clutch
+customized for the curl project to Virtuozzo Application Platform cloud
+service.
+
+# Installation
+
+The build scripts require Python 3 (ver. 3.8 or higher) with the `pip` package,
+as well as `git`.
+
+Run `./builddeployment` to create `testclutch_deploy.tar.xz`. This assumes the
+testclutch source is in the directory `../testclutch/` and the `master` branch
+is the version to deploy. To change these, run the script with the `GITREPO`
+and `TAG` variables pointing to a git repository location (local or remote) and
+branch or tag, respectively.
+
+# Environment Setup
+
+The deployment bundle is designed to be deployed on Virtuozzo Application
+Platform (formerly Jelastic), a container orchestration service. Create a new
+environment from scratch by following these steps:
+
+0. Choose *NEW ENVIRONMENT*
+0. At the top of the environment topology diagram, click *SSL* and *enable wildcard SSL*
+0. in the *Application* block, choose *Apache Python*
+    - tested version: Apache 2.4.58
+    - tested version: Python 3.11.8
+0. Under *Application Servers*:
+    - 1 reserved cloudlet, 1 scaling limit, 1 horizontal scaling
+    - horizontal scaling: stateless
+    - disk limit 5 GB
+    - access via SLB: ON
+    - Public IPv4: OFF
+    - Environment Name: &lt;unique hostname>
+0. In the *Storage Containers* block, choose *Shared Storage*
+    - tested version: NFS 2.0-9.6
+    - 1 reserved cloudlet, 1 scaling limit, 1 horizontal scaling
+    - horizontal scaling: stateless
+    - auto-clustering: OFF
+    - access via SLB: OFF
+    - disk limit 15 GB
+0. In *Application Servers*, *Settings→Volumes*
+    - Choose Add, Data Container, Shared Storage node, click checkbox for
+      `/data /data`, then *Add*, *Apply*
+
+# Deployment
+
+In the *Deployment Manager*, upload `testclutch_deploy.tar.xz` and choose the
+*Deploy to...* option to deploy to the application server just created. Set the
+*Post deploy* hook to:
+```sh
+#!/bin/bash
+exec /var/www/webroot/ROOT/application/bin/post-deploy
+```
+
+# Redeployment
+
+To update the software, just upload a new bundle and deploy it as before. The
+*Post deploy* hook should remain the same as before.
+
+When using the *Redeploy* option to updates the application servers, ensure
+that *Keep volume data* is ON. If redeployment happens with that off, the
+application code will be gone and there will be a site outage until you deploy
+a bundle again.
+
+# Tokens
+
+GitHub requires an access token for Test Clutch to download GitHub Actions
+logs. Create one by going to https://github.com/settings/tokens and choosing
+fine-grained tokens. Create a new token with these characteristics:
+
+  - Public repository access (no other special fine-grained access is needed)
+
+Copy the token contents from the web browser and store it in a file called
+`ghatoken` in a protected location on your local machine. Use the *Application
+Servers - Config* menu in Virtuozzo to upload that file to the container in the
+location `/data/auth/ghatoken`. Once the file is there, the periodic update job
+will start downloading GitHub Actions logs.
+
+# Logs
+
+Application logs are stored in the journal and may be downloaded using the
+*Application Servers: Log* menu. Navigate to `/journal/X` (where `X` is a
+unique identifier) and download the journal files desired. The files are
+automatically rotated.
+
+## Author
+
+Copyright (c) 2024 Dan Fandrich <dan@coneharvesters.com>
+Licensed under the MIT license (see the file [LICENSE](LICENSE) for details)
---
+++ distfiles/wsgi.py
@@ -0,0 +1,108 @@
+"""WSGI server for returning static pages.
+
+The deployment expects a WSGI module, so that's what we give it, even if
+we're not using it to its potential (yet).
+"""
+
+import base64
+import datetime
+import mimetypes
+import os
+import re
+import struct
+import sys
+from typing import List, Tuple
+
+# Number of hours to use in the expires header
+EXPIRE_HOURS = 1
+
+
+def root_path():
+    "Return the directory containing this very module"
+    return os.path.dirname(sys.modules[__name__].__file__)
+
+
+sys.path.append(os.path.join(root_path(), 'application', 'python'))
+
+
+INDEX_PATH = 'index.html'
+CHARSET = 'UTF-8'
+SAFE_PATH = re.compile(r'^[-_/.a-zA-Z0-9]+$')
+
+
+def get_static_path(fn: str) -> str:
+    return os.path.join(root_path(), fn.lstrip('/'))
+
+
+def get_static(fn: str) -> bytes:
+    try:
+        with open(get_static_path(fn), 'rb') as f:
+            output = f.read()
+    except FileNotFoundError:
+        output = 'ERROR: cannot find static file\n'.encode(CHARSET)
+    return output
+
+
+def get_static_headers(fn: str) -> List[Tuple[str, str]]:
+    response_headers = []
+    (mime, encoding) = mimetypes.guess_type(get_static_path(fn))
+    if mime:
+        response_headers.append(('Content-Type', f'{mime}; charset={CHARSET}'))
+
+    stat = os.stat(get_static_path(fn))
+    mtime = stat.st_mtime
+    mtimestr = datetime.datetime.fromtimestamp(mtime).strftime(
+            '%a, %d %b %Y %H:%M:%S GMT')
+    response_headers.append(('Last-Modified', mtimestr))
+
+    expstr = (datetime.datetime.now(tz=datetime.timezone.utc)
+              + datetime.timedelta(hours=EXPIRE_HOURS)).strftime('%a, %d %b %Y %H:%M:%S GMT')
+    response_headers.append(('Expires', expstr))
+
+    # Create an ETag based on size, mtime and inode number
+    tagval = struct.pack('>qqq', stat.st_size, int(stat.st_mtime), stat.st_ino)
+    tagval = tagval.lstrip(b'\0')  # These left-justified zeros from the size add only bulk
+    response_headers.append(('ETag', '"' + base64.b85encode(tagval).decode('US-ASCII') + '"'))
+    return response_headers
+
+
+def safe_path(path: str) -> bool:
+    """Only allow safe paths to be served.
+
+    In production, this can just be "return path == '/'" because all the other "safe" paths are
+    served directly by Apache, but serving the rest here as well makes testing easier.
+    This goes a bit overboard in checking for safety since the WSGI server does
+    much of the sanity checking for us. But, it's better to be safe than sorry (we really don't
+    want to accidentally serve /etc/passwd).
+    """
+    parts = path.split('/', 2)
+    return (parts[0] == ''
+            and parts[1] in frozenset(('index.html', 'images', 'static', 'robots.txt'))
+            and path.count('.') <= 1 and path.count('/') <= 2 and SAFE_PATH.match(path) is not None
+            and os.path.isfile(get_static_path(path)))
+
+
+def application(environ, start_response) -> List[bytes]:
+    path = environ['PATH_INFO']
+    if path == '/':
+        path = '/' + INDEX_PATH
+    if safe_path(path):
+        status = '200 OK'
+        response_headers = get_static_headers(path)
+        start_response(status, response_headers)
+        output = get_static(path)
+    else:
+        status = '404 Not found'
+        response_headers = [('Content-Type', f'text/plain; charset={CHARSET}')]
+        start_response(status, response_headers)
+        output = 'Page not found\n'.encode(CHARSET)
+    return [output]
+
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    with make_server("", 8000, application) as httpd:
+        print("Serving on port 8000...")
+
+        # Serve until process is killed
+        httpd.serve_forever()
